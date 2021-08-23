52655427c8f295819e83f126f5955245bccf60c6
Aarush Tanwar <tanwaraarush007@gmail.com>
2021-08-23 16:18:53 +0530

    styled about us component

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection.jsx'
 import ServicesSection from '../components/ServicesSection.jsx';
+import FaqSection from '../components/FaqSection.jsx';
 
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </div>
   );
 }
