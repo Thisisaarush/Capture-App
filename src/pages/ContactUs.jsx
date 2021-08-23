7f0d9b12c5db4cef6cc92927203b8f4abcd7b20a
Aarush Tanwar <tanwaraarush007@gmail.com>
2021-08-24 01:17:55 +0530

    fixed some weired bugs with animations

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,20 +1,82 @@
 import React from "react";
 
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation.js';
+import { pageAnimation, titleAnim } from '../animation.js';
+import styled from "styled-components";
+
 
 const ContactUs = () => {
   return (
-    <motion.div 
+    <ContactStyle 
       variants={pageAnimation} 
       initial='hidden' 
       animate='show' 
       exit='exit'
       style= {{background: '#fff'}}
     >
-      <h1>Contact</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send Us A Message</h2>
+          </Social>
+        </Hide>
+
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send an Email</h2>
+          </Social>
+        </Hide>
+
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Social Media Connect</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 }
+
+// styled components
+
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+    cursor: pointer;
+  }
+`;
 
 export default ContactUs;
--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -8,8 +8,15 @@ import goodtimes from '../img/goodtimes-small.png';
 
 import { motion } from 'framer-motion';
 import { pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer } from '../animation.js';
+import { useScroll } from '../components/useScroll';
+import ScrollTop from "../components/ScrollTop";
+
+
 
 const OurWork = () => {
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
+
   return (
     <Work 
       variants={pageAnimation} 
@@ -34,20 +41,22 @@ const OurWork = () => {
           </Hide>
         </Link>
       </Movie>
-      <Movie>
+      <Movie ref={element} variants={fade} animate={controls} initial='hidden'>
         <h2>The Racer</h2>
         <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to='/work/the-racer'>
           <img src={theracer} alt="racer" />
         </Link>
       </Movie>
-      <Movie>
+      <Movie ref={element2} variants={fade} animate={controls2} initial='hidden'>
         <h2>Good Times</h2>
         <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to='/work/good-times'>
           <img src={goodtimes} alt="goodtimes" />
         </Link>
       </Movie>
+
+      <ScrollTop />
 
     </Work>
   );
@@ -65,7 +74,7 @@ const Work = styled(motion.div)`
   }
 `;
 
-const Movie = styled.div`
+const Movie = styled(motion.div)`
   padding-bottom: 10rem;
 
   .line {
