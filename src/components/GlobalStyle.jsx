4261c42c8a968d2e1a4ce6c43a94416d043a0731
Aarush Tanwar <tanwaraarush007@gmail.com>
2021-08-23 16:43:19 +0530

    styled services component

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -10,7 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     background: #1b1b1b;
-
+    font-family: 'Inter', sans-serif;
   }
 
   button {
@@ -22,6 +22,7 @@ const GlobalStyle = createGlobalStyle`
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: 'Inter', sans-serif;
 
     &:hover {
       background-color: #23d997;
