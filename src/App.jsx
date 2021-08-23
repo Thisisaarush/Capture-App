22c05f32c3126b6e01fe5e1501d68e518a775c0d
Aarush Tanwar <tanwaraarush007@gmail.com>
2021-08-23 15:33:09 +0530

    added aboutus and services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      
+      <AboutUs />
     </div>
   );
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection.jsx'
+import ServicesSection from '../components/ServicesSection.jsx';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+      <ServicesSection />
+    </div>
+  );
+}
+
+export default AboutUs;
