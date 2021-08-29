8867eabab287266944c3c56cc15e265239248b60
Aarush Tanwar <tanwaraarush007@gmail.com>
2021-08-29 23:06:03 +0530

    Update MovieDetail.jsx

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -21,7 +21,7 @@ const Award = ({ title, description }) => {
 const MovieDetail = () => {
   const history = useHistory();
   const url = history.location.pathname;
-  const [movies, setMovies] = useState(MovieState);
+  const [movies] = useState(MovieState);
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
